446ba02fb035e737b09df48f1025a04a1c4d6599
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-03-11 16:28:01 +0300

    refactor

---
+++ src/AspnetRun.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using AspnetRun.Core.Interfaces;
+using AspnetRun.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Infrastructure.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AspnetRunContext _context;
+
+        public UnitOfWork(AspnetRunContext context)
+        {
+            _context = context;
+        }
+
+        public bool Commit()
+        {
+            return _context.SaveChanges() > 0;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
