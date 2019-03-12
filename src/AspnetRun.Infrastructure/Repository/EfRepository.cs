27b76d35770e435daf82891b0b4ec3650d80fc91
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-03-12 11:06:24 +0300

    RepositoryBase implemented

--- src/AspnetRun.Infrastructure/Repository/EfRepository.cs
+++ src/AspnetRun.Infrastructure/Repository/EfRepository.cs
@@ -1,43 +1,48 @@
-﻿using AspnetRun.Core.Interfaces;
+﻿using AspnetRun.Core.Entities;
+using AspnetRun.Core.Interfaces;
+using AspnetRun.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace AspnetRun.Infrastructure.Repository
 {
-    public class EfRepository<T> // : IRepository<T>, IAsyncRepository<T> where T : Entity
+    public class EfRepository<T> : IRepository<T>, IAsyncRepository<T> where T : Entity
     {
-        //protected readonly AlfonsoContext _dbContext;
+        protected readonly AspnetRunContext _dbContext;
 
-        //public EfRepository(AlfonsoContext dbContext)
-        //{
-        //    _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
-        //}
+        public EfRepository(AspnetRunContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
 
-        //public virtual T GetById(int id)
-        //{
-        //    return _dbContext.Set<T>().Find(id);
-        //}
+        public virtual T GetById(int id)
+        {
+            return _dbContext.Set<T>().Find(id);
+        }
 
-        //public virtual async Task<T> GetByIdAsync(int id)
-        //{
-        //    return await _dbContext.Set<T>().FindAsync(id);
-        //}
+        public virtual async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
 
         //public T GetSingleBySpec(ISpecification<T> spec)
         //{
         //    return List(spec).FirstOrDefault();
         //}
 
-        //public IEnumerable<T> ListAll()
-        //{
-        //    return _dbContext.Set<T>().AsEnumerable();
-        //}
+        public IEnumerable<T> ListAll()
+        {
+            return _dbContext.Set<T>().AsEnumerable();
+        }
 
-        //public async Task<IReadOnlyList<T>> ListAllAsync()
-        //{
-        //    return await _dbContext.Set<T>().ToListAsync();
-        //}
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
 
         //public IEnumerable<T> List(ISpecification<T> spec)
         //{
@@ -59,45 +64,45 @@ namespace AspnetRun.Infrastructure.Repository
         //    return await ApplySpecification(spec).CountAsync();
         //}
 
-        //public T Add(T entity)
-        //{
-        //    _dbContext.Set<T>().Add(entity);
-        //    _dbContext.SaveChanges();
+        public T Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            _dbContext.SaveChanges();
 
-        //    return entity;
-        //}
+            return entity;
+        }
 
-        //public async Task<T> AddAsync(T entity)
-        //{
-        //    _dbContext.Set<T>().Add(entity);
-        //    await _dbContext.SaveChangesAsync();
+        public async Task<T> AddAsync(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            await _dbContext.SaveChangesAsync();
 
-        //    return entity;
-        //}
+            return entity;
+        }
 
-        //public void Update(T entity)
-        //{
-        //    _dbContext.Entry(entity).State = EntityState.Modified;
-        //    _dbContext.SaveChanges();
-        //}
+        public void Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
 
-        //public async Task UpdateAsync(T entity)
-        //{
-        //    _dbContext.Entry(entity).State = EntityState.Modified;
-        //    await _dbContext.SaveChangesAsync();
-        //}
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
 
-        //public void Delete(T entity)
-        //{
-        //    _dbContext.Set<T>().Remove(entity);
-        //    _dbContext.SaveChanges();
-        //}
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            _dbContext.SaveChanges();
+        }
 
-        //public async Task DeleteAsync(T entity)
-        //{
-        //    _dbContext.Set<T>().Remove(entity);
-        //    await _dbContext.SaveChangesAsync();
-        //}
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
 
         //private IQueryable<T> ApplySpecification(ISpecification<T> spec)
         //{
