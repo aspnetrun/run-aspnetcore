4172f7f6e9b98b948e0bd5251cf0867dc277e915
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-03-13 09:15:56 +0300

    test page finished

---
+++ test/AspnetRun.Web.Tests/Pages/IndexPageOnGet.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspnetRun.Web.Tests.Pages
+{
+    public class IndexPageOnGet : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        public HttpClient Client { get; }
+
+        public IndexPageOnGet(CustomWebApplicationFactory<Startup> factory)
+        {
+            Client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnsIndexPageWithProductListing()
+        {
+            // Arrange & Act
+            var response = await Client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Contains("product", stringResponse);
+        }
+    }
+}
