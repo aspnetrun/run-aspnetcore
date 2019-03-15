86510436fa5b0332ae360aa6f7d7f755258226b1
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-03-15 21:59:13 +0300

    IHaveCustomMapping

--- src/AspnetRun.Application/Infrastructure/AutoMapper/MapperProfileHelper.cs
+++ src/AspnetRun.Application/Infrastructure/AutoMapper/MapperProfileHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AspnetRun.Application.Interfaces.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
---
+++ src/AspnetRun.Application/Interfaces/Mapping/IMapTo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Application.Interfaces.Mapping
+{
+    public interface IMapTo<TEntity>
+    {
+    }
+}
