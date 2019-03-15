d5009a9a4a64ad9fd1c0bc170501fab928aa501f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-03-15 21:57:53 +0300

    IHaveCustomMapping

---
+++ src/AspnetRun.Application/Interfaces/Mapping/IHaveCustomMapping.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Application.Interfaces.Mapping
+{
+    public interface IHaveCustomMapping
+    {
+        void CreateMappings(Profile configuration);
+    }
+}
