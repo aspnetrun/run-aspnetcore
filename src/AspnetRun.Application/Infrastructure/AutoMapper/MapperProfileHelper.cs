e7cb606dae258541f08e4d61362bbafc287dda47
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-03-15 21:56:38 +0300

    MapperProfileHelper added

---
+++ src/AspnetRun.Application/Infrastructure/AutoMapper/MapperProfileHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace AspnetRun.Application.Infrastructure.AutoMapper
+{
+    public sealed class Map
+    {
+        public Type Source { get; set; }
+        public Type Destination { get; set; }
+    }
+
+    public static class MapperProfileHelper
+    {
+        public static IList<Map> LoadStandardMappings(Assembly rootAssembly)
+        {
+            var types = rootAssembly.GetExportedTypes();
+
+            var mapsFrom = (
+                    from type in types
+                    from instance in type.GetInterfaces()
+                    where
+                        instance.IsGenericType && instance.GetGenericTypeDefinition() == typeof(IMapFrom<>) &&
+                        !type.IsAbstract &&
+                        !type.IsInterface
+                    select new Map
+                    {
+                        Source = type.GetInterfaces().First().GetGenericArguments().First(),
+                        Destination = type
+                    }).ToList();
+
+            return mapsFrom;
+        }
+
+        public static IList<IHaveCustomMapping> LoadCustomMappings(Assembly rootAssembly)
+        {
+            var types = rootAssembly.GetExportedTypes();
+
+            var mapsFrom = (
+                    from type in types
+                    from instance in type.GetInterfaces()
+                    where
+                        typeof(IHaveCustomMapping).IsAssignableFrom(type) &&
+                        !type.IsAbstract &&
+                        !type.IsInterface
+                    select (IHaveCustomMapping)Activator.CreateInstance(type)).ToList();
+
+            return mapsFrom;
+        }
+    }
+}
