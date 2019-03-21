74df66b0e72cf64219a959b7fc9c70435117a9e7
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-03-21 11:06:57 +0300

    test projects fixed

--- test/AspnetRun.Web.Tests/Pages/IndexPageOnGet.cs
+++ test/AspnetRun.Web.Tests/Pages/IndexPageOnGet.cs
@@ -1,28 +1,28 @@
-﻿//using System.Net.Http;
-//using System.Threading.Tasks;
-//using Xunit;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
 
-//namespace AspnetRun.Web.Tests.Pages
-//{
-//    public class IndexPageOnGet : IClassFixture<CustomWebApplicationFactory<Startup>>
-//    {
-//        public HttpClient Client { get; }
+namespace AspnetRun.Web.Tests.Pages
+{
+    public class IndexPageOnGet : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        public HttpClient Client { get; }
 
-//        public IndexPageOnGet(CustomWebApplicationFactory<Startup> factory)
-//        {
-//            Client = factory.CreateClient();
-//        }
+        public IndexPageOnGet(CustomWebApplicationFactory<Startup> factory)
+        {
+            Client = factory.CreateClient();
+        }
 
-//        [Fact]
-//        public async Task ReturnsIndexPageWithProductListing()
-//        {
-//            // Arrange & Act
-//            var response = await Client.GetAsync("/");
-//            response.EnsureSuccessStatusCode();
-//            var stringResponse = await response.Content.ReadAsStringAsync();
+        [Fact]
+        public async Task ReturnsIndexPageWithProductListing()
+        {
+            // Arrange & Act
+            var response = await Client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
 
-//            // Assert
-//            Assert.Contains("product", stringResponse);
-//        }
-//    }
-//}
+            // Assert
+            Assert.Contains("product", stringResponse);
+        }
+    }
+}
