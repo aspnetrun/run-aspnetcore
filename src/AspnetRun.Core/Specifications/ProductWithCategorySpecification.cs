1a7f32f228255d64dea95d125e296feb95f9703e
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-21 14:31:37 +0300

    ProductWithCategorySpecification

---
+++ src/AspnetRun.Core/Specifications/ProductWithCategorySpecification.cs
@@ -0,0 +1,18 @@
+﻿using AspnetRun.Core.Entities;
+
+namespace AspnetRun.Core.Specifications
+{
+    public class ProductWithCategorySpecification : BaseSpecification<Product>
+    {
+        public ProductWithCategorySpecification(string productName) 
+            : base(p => p.ProductName.ToLower().Contains(productName.ToLower()))
+        {
+            AddInclude(p => p.Category);
+        }
+
+        public ProductWithCategorySpecification() : base(null)            
+        {
+            AddInclude(p => p.Category);
+        }
+    }
+}
