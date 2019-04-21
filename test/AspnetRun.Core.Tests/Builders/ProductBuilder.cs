ac27b85af0f5df12acc1a365a3cd807456c4b300
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-21 16:04:50 +0300

    Matches_Product_With_Category_Spec

--- test/AspnetRun.Core.Tests/Builders/ProductBuilder.cs
+++ test/AspnetRun.Core.Tests/Builders/ProductBuilder.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using AspnetRun.Core.Entities;
 using System.Collections.Generic;
-using System.Text;
 
 namespace AspnetRun.Core.Tests.Builders
 {
     public class ProductBuilder
     {
+        public int ProductId1 => 123;
+        public int ProductId2 => 124;
+        public int ProductId3 => 125;
+        public string ProductName1 => "ProductX";
+        public string ProductName2 => "ProductY";
+        public string ProductName3 => "ProductZ";
 
+        public List<Product> GetProductCollection()
+        {
+            return new List<Product>()
+            {
+                Product.Create(ProductId1, 1, ProductName1),
+                Product.Create(ProductId2, 1, ProductName2),
+                Product.Create(ProductId3, 1, ProductName3)
+            };
+        }
     }
 }
--- test/AspnetRun.Core.Tests/Specifications/ProductSpecificationTests.cs
+++ test/AspnetRun.Core.Tests/Specifications/ProductSpecificationTests.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using AspnetRun.Core.Specifications;
+using AspnetRun.Core.Tests.Builders;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Xunit;
 
 namespace AspnetRun.Core.Tests.Specifications
 {
     public class ProductSpecificationTests
     {
+        private ProductBuilder ProductBuilder { get; } = new ProductBuilder();
 
+        [Fact]
+        public void Matches_Product_With_Category_Spec()
+        {
+            var spec = new ProductWithCategorySpecification(ProductBuilder.ProductName1);
+
+            var result = ProductBuilder.GetProductCollection()
+                .AsQueryable()
+                .FirstOrDefault(spec.Criteria);
+
+            Assert.NotNull(result);
+            Assert.Equal(ProductBuilder.ProductId1, result.Id);
+        }
     }
 }
