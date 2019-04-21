7a45893d0112f0fc4ccd01772668b32c671dafd9
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-21 15:51:40 +0300

    ProductSpecificationTests

--- test/AspnetRun.Core.Tests/Specifications/ProductSpecificationTests.cs
+++ test/AspnetRun.Core.Tests/Specifications/ProductSpecificationTests.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace AspnetRun.Core.Tests.Specifications
 {
-    public class ProductWithCategorySpecificationTests
+    public class ProductSpecificationTests
     {
 
     }
