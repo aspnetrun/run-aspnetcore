5a54d061ef3f8c73f0ab6974d0eeaf8818a4db11
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-20 13:18:18 +0300

    AspnetRunSettings added

---
+++ src/AspnetRun.Core/Configuration/AspnetRunSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Core.Configuration
+{
+    public class AspnetRunSettings
+    {
+        public string ConnectionString { get; set; }
+    }
+}
